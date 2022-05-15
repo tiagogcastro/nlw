0f86f3fe0727d944f959d3bbdfad7db7477a90ee
Tiaguin061 <tiaguin180@gmail.com>
2022-05-15 15:41:49 -0300

    feat: Created yup validation

---
+++ frontend/src/utils/yupGetValidation.ts
@@ -0,0 +1,17 @@
+import { ValidationError } from 'yup';
+
+export interface Errors {
+  [key: string]: string;
+}
+
+export function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach((error) => {
+    if(error.path) {
+      validationErrors[error.path] = error.message;
+    }
+  });
+
+  return validationErrors;  
+}
